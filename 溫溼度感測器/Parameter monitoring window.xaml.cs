5140930d9a5b3f2e55502564e8ea8dabde8468bc
Waiting <Waiting@DESKTOP-QKBTLEQ>
2024-01-15 23:52:13 +0800

    完成基礎前端頁面

---
+++ 溫溼度感測器/Parameter monitoring window.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 溫溼度感測器
+{
+    /// <summary>
+    /// Parameter_monitoring_window.xaml 的互動邏輯
+    /// </summary>
+    public partial class Parameter_monitoring_window : UserControl
+    {
+        public Parameter_monitoring_window()
+        {
+            InitializeComponent();
+        }
+    }
+}
